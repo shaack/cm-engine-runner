049d1060efc9383e76dfea204f67ddd27b121724
shaack <shaack@gmail.com>
2021-12-13 22:34:51 +0100

    renamed to cm-engine-runner

--- src/cm-engine-runner/EngineRunner.js
+++ src/cm-engine-runner/EngineRunner.js
@@ -1,6 +1,6 @@
 /**
  * Author and copyright: Stefan Haack (https://shaack.com)
- * Repository: https://github.com/shaack/cm-chess-engine-runner
+ * Repository: https://github.com/shaack/cm-engine-runner
  * License: MIT, see file 'LICENSE'
  */
 
